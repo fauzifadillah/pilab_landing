8f8c59caf01a6b13a702aef454a847feada77c24
user <user@users-MacBook-Pro-5074.local>
2020-04-12 23:52:25 +0700

    footer, cookie, head meta

--- components/TheFooter.vue
+++ components/TheFooter.vue
@@ -2,52 +2,87 @@
   <footer class="bg-white">
     <div class="container mx-auto px-8">
       <div class="w-full flex flex-col md:flex-row py-6">
-        <div class="flex-1 mb-6">
+        <div class="flex-auto mb-6">
           <!-- <logo /> -->
           <img id="footer-image" src="~assets/img/footer.png"/>
+          <p class="uppercase text-gray-700 mt-6 mr-4 md:mb-1 text-base	">PT. PADJADJARAN TEKNOLOGI INFORMATIKA</p>
+          <p class=" text-gray-600 mr-4 text-sm	">Kawasan Sains dan Teknologi Padjadjaran</p>
+          <p class=" text-gray-600 mr-4 text-sm	">Jl. Raya Bandung-Sumedang KM. 21 Universitas Padjadjaran<br>Jawa Barat, Indonesia 45363</p>
+          <p class=" text-gray-600 mr-4 md:mt-2 text-sm	">code@padjadjaranlab.com</p>
+          <p class=" text-gray-600 mr-4 text-sm	">+62 859 4722 4853</p>
         </div>
 
-        <div class="flex-1">
-          <p class="uppercase text-gray-500 md:mb-6">Links</p>
+        <div class="flex-auto">
+          <p class="uppercase text-gray-500 md:mb-3">Links</p>
           <ul class="list-reset mb-6">
             <li class="mt-2 inline-block mr-2 md:block md:mr-0">
               <a
                 href="#"
-                class="no-underline hover:underline text-gray-800 hover:text-orange-500"
-              >FAQ</a>
+                class="text-sm no-underline hover:underline text-gray-800 hover:text-orange-500"
+              >Services</a>
             </li>
             <li class="mt-2 inline-block mr-2 md:block md:mr-0">
               <a
                 href="#"
-                class="no-underline hover:underline text-gray-800 hover:text-orange-500"
-              >Help</a>
+                class="text-sm no-underline hover:underline text-gray-800 hover:text-orange-500"
+              >Portofolio</a>
             </li>
             <li class="mt-2 inline-block mr-2 md:block md:mr-0">
               <a
                 href="#"
-                class="no-underline hover:underline text-gray-800 hover:text-orange-500"
-              >Support</a>
+                class="text-sm no-underline hover:underline text-gray-800 hover:text-orange-500"
+              >Approach</a>
+            </li>
+            <li class="mt-2 inline-block mr-2 md:block md:mr-0">
+              <a
+                href="#"
+                class="text-sm no-underline hover:underline text-gray-800 hover:text-orange-500"
+              >Profiles</a>
+            </li>
+            <li class="mt-2 inline-block mr-2 md:block md:mr-0">
+              <a
+                href="#"
+                class="text-sm no-underline hover:underline text-gray-800 hover:text-orange-500"
+              >Talk</a>
             </li>
           </ul>
         </div>
-        <div class="flex-1">
-          <p class="uppercase text-gray-500 md:mb-6">Legal</p>
+        <div class="flex-auto">
+          <p class="uppercase text-gray-500 md:mb-3">Links</p>
           <ul class="list-reset mb-6">
             <li class="mt-2 inline-block mr-2 md:block md:mr-0">
               <a
                 href="#"
-                class="no-underline hover:underline text-gray-800 hover:text-orange-500"
-              >Terms</a>
+                class="text-sm no-underline hover:underline text-gray-800 hover:text-orange-500"
+              >Services</a>
             </li>
             <li class="mt-2 inline-block mr-2 md:block md:mr-0">
               <a
                 href="#"
-                class="no-underline hover:underline text-gray-800 hover:text-orange-500"
-              >Privacy</a>
+                class="text-sm no-underline hover:underline text-gray-800 hover:text-orange-500"
+              >Portofolio</a>
+            </li>
+            <li class="mt-2 inline-block mr-2 md:block md:mr-0">
+              <a
+                href="#"
+                class="text-sm no-underline hover:underline text-gray-800 hover:text-orange-500"
+              >Approach</a>
+            </li>
+            <li class="mt-2 inline-block mr-2 md:block md:mr-0">
+              <a
+                href="#"
+                class="text-sm no-underline hover:underline text-gray-800 hover:text-orange-500"
+              >Profiles</a>
+            </li>
+            <li class="mt-2 inline-block mr-2 md:block md:mr-0">
+              <a
+                href="#"
+                class="text-sm no-underline hover:underline text-gray-800 hover:text-orange-500"
+              >Talk</a>
             </li>
           </ul>
         </div>
-        <div class="flex-1">
+        <div class="flex-auto">
           <p class="uppercase text-gray-500 md:mb-6">Social</p>
           <ul class="list-reset mb-6">
             <li class="mt-2 inline-block mr-2 md:block md:mr-0">
@@ -70,35 +105,9 @@
             </li>
           </ul>
         </div>
-        <div class="flex-1">
-          <p class="uppercase text-gray-500 md:mb-6">Company</p>
-          <ul class="list-reset mb-6">
-            <li class="mt-2 inline-block mr-2 md:block md:mr-0">
-              <a
-                href="#"
-                class="no-underline hover:underline text-gray-800 hover:text-orange-500"
-              >Official Blog</a>
-            </li>
-            <li class="mt-2 inline-block mr-2 md:block md:mr-0">
-              <a
-                href="#"
-                class="no-underline hover:underline text-gray-800 hover:text-orange-500"
-              >About Us</a>
-            </li>
-            <li class="mt-2 inline-block mr-2 md:block md:mr-0">
-              <a
-                href="#"
-                class="no-underline hover:underline text-gray-800 hover:text-orange-500"
-              >Contact</a>
-            </li>
-          </ul>
-        </div>
       </div>
     </div>
 
-    <a
-      href="https://www.freepik.com/free-photos-vectors/background"
-    >Background vector created by freepik - www.freepik.com</a>
   </footer>
 </template>
 <style scoped>
---
+++ pages/projects.vue
@@ -0,0 +1,397 @@
+<template>
+  <div>
+<div class="pilab"></div>      
+<div class="porto-bigdat"></div>
+    <section class="bg-white border-b py-8">
+        <div class="container mx-auto flex flex-wrap pt-4 pb-12">
+            <h6 class="w-full my-2 text-2xl font-light leading-tight text-center text-gray-800">Dashboard</h6>
+            <h3 class="w-full my-2 text-5xl font-medium leading-tight text-center text-gray-800"> Big Data Analytics</h3>
+            <h5 class="w-full my-2 text-2xl font-light leading-tight text-center text-gray-800"></h5>
+        </div>
+    </section> 
+<div class="porto-ecomm"></div>
+    <section class="bg-white border-b py-8">
+        <div class="container mx-auto flex flex-wrap pt-4 pb-12">
+            <h6 class="w-full my-2 text-2xl font-light leading-tight text-center text-gray-800">Multi-vendor + Bank & GoPay Billing System</h6>
+            <h3 class="w-full my-2 text-5xl font-medium leading-tight text-center text-gray-800"> E-Commerce</h3>
+            <h5 class="w-full my-2 text-2xl font-light leading-tight text-center text-gray-800"></h5>
+        </div>
+    </section> 
+<div class="porto-event"></div>
+    <section class="bg-white border-b py-8">
+        <div class="container mx-auto flex flex-wrap pt-4 pb-12">
+            <h6 class="w-full my-2 text-2xl font-light leading-tight text-center text-gray-800">Confrence System & Landing Page</h6>
+            <h3 class="w-full my-2 text-5xl font-medium leading-tight text-center text-gray-800"> Events</h3>
+            <h5 class="w-full my-2 text-2xl font-light leading-tight text-center text-gray-800"></h5>
+        </div>
+    </section>
+<div class="porto-graf"></div>
+    <section class="bg-white border-b py-8">
+        <div class="container mx-auto flex flex-wrap pt-4 pb-12">
+            <h6 class="w-full my-2 text-2xl font-light leading-tight text-center text-gray-800">Energy Monitoring & Control System (Iaas)</h6>
+            <h3 class="w-full my-2 text-5xl font-medium leading-tight text-center text-gray-800">IoT Dashboard</h3>
+            <h5 class="w-full my-2 text-2xl font-light leading-tight text-center text-gray-800"></h5>
+        </div>
+    </section>
+<div class="porto-invent"></div>
+    <section class="bg-white border-b py-8">
+        <div class="container mx-auto flex flex-wrap pt-4 pb-12">
+            <h6 class="w-full my-2 text-2xl font-light leading-tight text-center text-gray-800">Stock, Performance, Invoice & Role</h6>
+            <h3 class="w-full my-2 text-5xl font-medium leading-tight text-center text-gray-800">Inventory System</h3>
+            <h5 class="w-full my-2 text-2xl font-light leading-tight text-center text-gray-800"></h5>
+        </div>
+    </section>
+<div class="porto-invent2"></div>
+    <section class="bg-white border-b py-8">
+        <div class="container mx-auto flex flex-wrap pt-4 pb-12">
+            <h6 class="w-full my-2 text-2xl font-light leading-tight text-center text-gray-800">Location Track, Geofencing, Barcode Scanner & Queuing System</h6>
+            <h3 class="w-full my-2 text-5xl font-medium leading-tight text-center text-gray-800">Inventory System v2</h3>
+            <h5 class="w-full my-2 text-2xl font-light leading-tight text-center text-gray-800"></h5>
+        </div>
+    </section>
+<div class="porto-lumea"></div>
+    <section class="bg-white border-b py-8">
+        <div class="container mx-auto flex flex-wrap pt-4 pb-12">
+            <h6 class="w-full my-2 text-2xl font-light leading-tight text-center text-gray-800">Store, Multi-category, multi-role, payment gateway, REST-API</h6>
+            <h3 class="w-full my-2 text-5xl font-medium leading-tight text-center text-gray-800">E-Commerce Single-vendor</h3>
+            <h5 class="w-full my-2 text-2xl font-light leading-tight text-center text-gray-800"></h5>
+        </div>
+    </section>
+<div class="porto-mobile"></div>
+    <section class="bg-white border-b py-8">
+        <div class="container mx-auto flex flex-wrap pt-4 pb-12">
+            <h6 class="w-full my-2 text-2xl font-light leading-tight text-center text-gray-800">Home-applicants control system (websocket, MQTT, MERN)</h6>
+            <h3 class="w-full my-2 text-5xl font-medium leading-tight text-center text-gray-800">IoT Controller Native Apps</h3>
+            <h5 class="w-full my-2 text-2xl font-light leading-tight text-center text-gray-800"></h5>
+        </div>
+    </section>
+<div class="porto-monitor"></div>
+    <section class="bg-white border-b py-8">
+        <div class="container mx-auto flex flex-wrap pt-4 pb-12">
+            <h6 class="w-full my-2 text-2xl font-light leading-tight text-center text-gray-800">Energy Monitoring System</h6>
+            <h3 class="w-full my-2 text-5xl font-medium leading-tight text-center text-gray-800">Smart-Dashboard</h3>
+            <h5 class="w-full my-2 text-2xl font-light leading-tight text-center text-gray-800"></h5>
+        </div>
+    </section>
+<div class="porto-printg"></div>
+    <section class="bg-white border-b py-8">
+        <div class="container mx-auto flex flex-wrap pt-4 pb-12">
+            <h6 class="w-full my-2 text-2xl font-light leading-tight text-center text-gray-800">Headless CMS, JAMStack</h6>
+            <h3 class="w-full my-2 text-5xl font-medium leading-tight text-center text-gray-800">Company Profile</h3>
+            <h5 class="w-full my-2 text-2xl font-light leading-tight text-center text-gray-800"></h5>
+        </div>
+    </section>
+<div class="porto-sparkui"></div>
+    <section class="bg-white border-b py-8">
+        <div class="container mx-auto flex flex-wrap pt-4 pb-12">
+            <h6 class="w-full my-2 text-2xl font-light leading-tight text-center text-gray-800">Apache Spark API Development on Hadoop</h6>
+            <h3 class="w-full my-2 text-5xl font-medium leading-tight text-center text-gray-800">Research</h3>
+            <h5 class="w-full my-2 text-2xl font-light leading-tight text-center text-gray-800"></h5>
+        </div>
+    </section>
+<div class="porto-timer"></div>
+    <section class="bg-white border-b py-8">
+        <div class="container mx-auto flex flex-wrap pt-4 pb-12">
+            <h6 class="w-full my-2 text-2xl font-light leading-tight text-center text-gray-800">Milli-second timer, upcoming events, alert & scheduler</h6>
+            <h3 class="w-full my-2 text-5xl font-medium leading-tight text-center text-gray-800">Interval Timer</h3>
+            <h5 class="w-full my-2 text-2xl font-light leading-tight text-center text-gray-800"></h5>
+        </div>
+    </section>
+<div class="pilab_blue"></div> 
+
+   
+
+
+    <!-- <hero /> -->
+    <!-- <about-pilab /> -->
+    <!-- <features /> -->
+    <!-- <intro-porto /> -->
+    <!-- <teasers/> -->
+    <!-- <div id="backvid"> -->
+  </div>
+</template>
+<style scoped>
+img {
+    width: 120px;
+  height: 120px;
+}
+.pilab {
+  /* The image used */  
+  background-image: url('~assets/img/porto/pilab_orange.png');
+
+  /* Full height */
+  min-height: 500px; 
+
+  /* Create the parallax scrolling effect */
+  background-attachment: fixed;
+  background-position: 75% 70%;
+  background-repeat: no-repeat;
+  background-size: cover;
+}
+.pilab_blue {
+  /* The image used */  
+  background-image: url('~assets/img/porto/pilab_blue.png');
+
+  /* Full height */
+  min-height: 500px; 
+
+  /* Create the parallax scrolling effect */
+  background-attachment: fixed;
+  background-position: 75% 70%;
+  background-repeat: no-repeat;
+  background-size: cover;
+}
+.porto-bigdat {
+  /* The image used */  
+  background-image: url('~assets/img/porto/bigdat.png');
+
+  /* Full height */
+  min-height: 500px; 
+
+  /* Create the parallax scrolling effect */
+  background-attachment: fixed;
+  background-position: center;
+  background-repeat: no-repeat;
+  background-size: cover;
+}
+
+.porto-ecomm {
+  /* The image used */  
+  background-image: url('~assets/img/porto/ecomm.png');
+
+  /* Full height */
+  min-height: 500px; 
+
+  /* Create the parallax scrolling effect */
+  background-attachment: fixed;
+  background-position: center;
+  background-repeat: no-repeat;
+  background-size: cover;
+}
+
+.porto-event {
+  /* The image used */  
+  background-image: url('~assets/img/porto/event.png');
+  /* Full height */
+  min-height: 500px; 
+
+  /* Create the parallax scrolling effect */
+  background-attachment: fixed;
+  background-position: center;
+  background-repeat: no-repeat;
+  background-size: cover;
+}
+.porto-graf {
+  /* The image used */  
+  background-image: url('~assets/img/porto/graf.png');
+  /* Full height */
+  min-height: 500px; 
+
+  /* Create the parallax scrolling effect */
+  background-attachment: fixed;
+  background-position: center;
+  background-repeat: no-repeat;
+  background-size: cover;
+}
+
+.porto-invent {
+  /* The image used */  
+  background-image: url('~assets/img/porto/invent.png');
+
+  /* Full height */
+  min-height: 500px; 
+
+  /* Create the parallax scrolling effect */
+  background-attachment: fixed;
+  background-position: center;
+  background-repeat: no-repeat;
+  background-size: cover;
+}
+
+.porto-invent2 {
+  /* The image used */  
+  background-image: url('~assets/img/porto/invent2.png');
+
+  /* Full height */
+  min-height: 500px; 
+
+  /* Create the parallax scrolling effect */
+  background-attachment: fixed;
+  background-position: center;
+  background-repeat: no-repeat;
+  background-size: cover;
+}
+.porto-lumea {
+  /* The image used */  
+  background-image: url('~assets/img/porto/lumea.png');
+
+  /* Full height */
+  min-height: 500px; 
+
+  /* Create the parallax scrolling effect */
+  background-attachment: fixed;
+  background-position: center;
+  background-repeat: no-repeat;
+  background-size: cover;
+}
+
+.porto-mobile {
+  /* The image used */  
+  background-image: url('~assets/img/porto/mobile.png');
+
+  /* Full height */
+  min-height: 500px; 
+
+  /* Create the parallax scrolling effect */
+  background-attachment: fixed;
+  background-position: center;
+  background-repeat: no-repeat;
+  background-size: cover;
+}
+
+.porto-monitor {
+  /* The image used */  
+  background-image: url('~assets/img/porto/monitor.png');
+
+  /* Full height */
+  min-height: 500px; 
+
+  /* Create the parallax scrolling effect */
+  background-attachment: fixed;
+  background-position: center;
+  background-repeat: no-repeat;
+  background-size: cover;
+}
+.porto-printg {
+  /* The image used */  
+  background-image: url('~assets/img/porto/printg.png');
+
+  /* Full height */
+  min-height: 500px; 
+
+  /* Create the parallax scrolling effect */
+  background-attachment: fixed;
+  background-position: center;
+  background-repeat: no-repeat;
+  background-size: cover;
+}
+
+.porto-timer {
+  /* The image used */  
+  background-image: url('~assets/img/porto/timer.png');
+
+  /* Full height */
+  min-height: 500px; 
+
+  /* Create the parallax scrolling effect */
+  background-attachment: fixed;
+  background-position: center;
+  background-repeat: no-repeat;
+  background-size: cover;
+}
+
+.porto-sparkui {
+  /* The image used */  
+  background-image: url('~assets/img/porto/sparkui.png');
+
+  /* Full height */
+  min-height: 500px; 
+
+  /* Create the parallax scrolling effect */
+  background-attachment: fixed;
+  background-position: center;
+  background-repeat: no-repeat;
+  background-size: cover;
+}
+
+.slick-track {
+    width: 13904px; opacity: 1; transform: translate3d(-6320px, 0px, 0px); transition: -webkit-transform 3000ms linear 0s;
+}
+
+</style>
+<style>
+@import 'vue-slick-carousel/dist/vue-slick-carousel.css';
+</style>
+<script>
+import VueSlickCarousel from 'vue-slick-carousel'
+import 'vue-slick-carousel/dist/vue-slick-carousel.css'
+ // optional style for arrows & dots
+import 'vue-slick-carousel/dist/vue-slick-carousel-theme.css'
+
+import Hero from '@/components/Hero'
+import Features from '@/components/Features'
+import Teasers from '@/components/Teasers'
+import Prices from '@/components/Prices'
+import CallToAction from '@/components/CallToAction'
+
+import about_pilab from '@/components/about_pilab'
+import about_pilab_right from '@/components/about_pilab_right'
+import about_pilab_left from '@/components/about_pilab_left'
+import servicecard from '@/components/service_card'
+import service_head_content from '@/components/service_head_content'
+import services_head_content_mirror from '@/components/services_head_content_mirror'
+import videos from '@/components/video'
+import TeaserColumn from '@/components/TeaserColumn'
+
+
+export default {
+    data () {
+        return {
+           settings: {
+            "arrows": false,
+            "dots": false,
+            "infinite": true,
+            "slidesToShow": 4,
+            "slidesToScroll": 1,
+            "autoplay": true,
+            "speed": 3000,
+            "autoplaySpeed": 0,
+            "cssEase": "linear",
+            "responsive": [
+                {
+                "breakpoint": 1024,
+                "settings": {
+                    "slidesToShow": 3,
+                    "slidesToScroll": 3,
+                    "infinite": true,
+                    "dots": true
+                }
+                },
+                {
+                "breakpoint": 600,
+                "settings": {
+                    "slidesToShow": 2,
+                    "slidesToScroll": 2,
+                    "initialSlide": 2
+                }
+                },
+                {
+                "breakpoint": 480,
+                "settings": {
+                    "slidesToShow": 1,
+                    "slidesToScroll": 1
+                }
+                }
+            ]
+            }      
+        }
+    },
+  name: 'servicespage',
+  components: {
+    hero: Hero,
+    features: Features,
+    teasers: Teasers,
+    prices: Prices,
+    'call-to-action': CallToAction,
+    'about-pilab': about_pilab,
+    about_pilab_right: about_pilab_right,
+    about_pilab_left: about_pilab_left,
+    'service-card': servicecard,
+    'service-head-content':service_head_content,
+    'services-head-content-mirror':services_head_content_mirror,
+    videos: videos,
+    'teaser-column': TeaserColumn,
+    VueSlickCarousel
+  }
+}
+</script>
