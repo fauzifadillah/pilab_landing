6a77a86cac7565c644c2364067237ecbf91d8612
user <user@users-MacBook-Pro-5074.local>
2020-04-16 01:07:05 +0700

    typo fix profiles

--- components/TheFooter.vue
+++ components/TheFooter.vue
@@ -65,7 +65,7 @@
             </li>
             <li class="mt-2 inline-block mr-2 md:block md:mr-0">
               <a
-                href="#"
+                href="https://linkedin.com/company/padjadjaran-lab"
                 class="no-underline hover:underline text-gray-800 hover:text-orange-500"
               >LinkedIn</a>
             </li>
